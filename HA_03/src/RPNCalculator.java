30b35e20da8dfd5338eaa17f2a7e9600314f8274
Julia <julia.langhammer7@gmail.com>
2018-04-26 20:35:00 +0200

    Started HA_03

---
+++ HA_03/src/RPNCalculator.java
@@ -0,0 +1,23 @@
+public interface RPNCalculator {
+
+    //TODO Grundrechenarten
+    //TODO Vorzeichenwechsel (+/-)
+    //TODO Fakultät
+    //TODO Löschen der Eingabe, löschen des obersten Wertes, alles löschen
+
+    public int add();
+
+    public int sub();
+
+    public int mul();
+
+    public int mod();
+
+    public int div();
+
+    public int getResult();
+
+    public void setResult(int result);
+
+
+}
